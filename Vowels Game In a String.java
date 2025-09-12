f1527836be3812ce1e74a5ecaf0980ce2556ab3f
Drishti2005 <142567480+Drishti2005@users.noreply.github.com>
2025-09-12 19:31:46 +0530

    Create Vowels Game In a String.java

---
+++ Vowels Game In a String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    c++;
+                }
+                
+            }
+        if(c==0){
+            return false;
+        }
+       
+        return true;
+    }
+}
