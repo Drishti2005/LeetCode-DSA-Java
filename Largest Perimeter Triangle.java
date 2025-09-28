084981263a7ff05b9d98b0e5239f4a61675b96a0
Drishti2005 <142567480+Drishti2005@users.noreply.github.com>
2025-09-28 14:25:03 +0530

    Largest Perimeter Triangle.java

---
+++ Largest Perimeter Triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        for(int i=nums.length-3;i>=0;i--){
+            int a=nums[i],b=nums[i+1],c=nums[i+2];
+            if(a+b>c){
+                return a+b+c;
+            }
+
+        }
+        return 0;
+    }
+}
